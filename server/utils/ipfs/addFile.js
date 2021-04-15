1d308a2d015a74b3d7559c4527e39f0cce3833c7
GITHUBXIONGXINOG <2017211677@stu.cqupt.edu.cn>
2021-04-15 20:29:45 +0800

    加密思路

--- server/utils/ipfs/addFile.js
+++ server/utils/ipfs/addFile.js
@@ -33,24 +33,38 @@ module.exports = {
                     const key = '0123456789abcdeffedcba9876543210' // Any string of 32 hexadecimal digits
                     // const originalData = fd
                     let encryptedData, decryptedData
-                    // console.log('fd:',fd);
+                    console.log('1-fd:',fd);
                     let jsonFd = JSON.stringify(fd)
-                    // console.log('jsonFd:',jsonFd);
+                    console.log('-----------------------------------------------');
+                    console.log('2-jsonFd:',jsonFd);
                     // ECB
                     encryptedData = SM4.encrypt(jsonFd, key, {
                         inputEncoding: 'utf8',
                         outputEncoding: 'base64'
                     })
-                    // console.log('encryptedData:',encryptedData);
-                    decryptedData = SM4.decrypt(encryptedData, key, {
+                    console.log('-----------------------------------------------');
+                    console.log('3-encryptedData:',encryptedData);
+
+                    const tempBuffer = Buffer.from(encryptedData)
+                    console.log('-----------------------------------------------');
+                    console.log('3.1-tempBuffer:',tempBuffer);
+                    const tempJSON = tempBuffer.toString()
+                    console.log('-----------------------------------------------');
+                    console.log('3.2-tempJSON:',tempJSON);
+
+                    decryptedData = SM4.decrypt(tempJSON, key, {
                         inputEncoding: 'base64',
                         outputEncoding: 'utf8'
                     })
-                    // console.log('decryptedData:',decryptedData);
+                    console.log('-----------------------------------------------');
+                    console.log('4-decryptedData:',decryptedData);
                     const jsonData = JSON.parse(decryptedData)
-                    // console.log(jsonData);
+                    console.log('-----------------------------------------------');
+                    console.log('5-jsonData',jsonData);
                     const decryptedDataBuffer = Buffer.from(jsonData)
-                    // console.log("decryptedDataBuffer:",decryptedDataBuffer);
+                    console.log('-----------------------------------------------');
+                    console.log("6-decryptedDataBuffer:",decryptedDataBuffer);
+
                     //拼接文件信息和文件内容
                     const content = Buffer.concat([fdInfoBuf, decryptedDataBuffer])
 
