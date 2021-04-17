4b4261968291e0cb601cd7601b4963d07c1393cc
GITHUBXIONGXINOG <2017211677@stu.cqupt.edu.cn>
2021-04-17 15:21:25 +0800

    封装ipfs连接/async/await方法

--- server/utils/ipfs/ipfs.js
+++ server/utils/ipfs/ipfs.js
@@ -1,9 +1,11 @@
 const ipfs_core = require('ipfs-core')
-const ipfs = async function(){
-    return new Promise(async(resolve,reject)=>{
-        const ipfs = await ipfs_core.create()
-        resolve(ipfs)
-    }) 
+const ipfs = async()=>{
+    const ipfs = await ipfs_core.create()
+    return ipfs
+    // return new Promise(async(resolve,reject)=>{
+    //     const ipfs = await ipfs_core.create()
+    //     resolve(ipfs)
+    // }) 
 }
 module.exports =  ipfs
 
